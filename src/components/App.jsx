c16db8ac3c26979f12c3695fc0e948bef36dc5cc
Volodymyr Hudzula <110738058+NeosFCDD@users.noreply.github.com>
2023-04-30 22:45:40 +0300

    add structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import Filter from "components/Form/Filter";
-import Form from "components/Form/Form";
-import Contacts from "components/Form/Contacts";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "components/Navigation/Navigation.jsx";
+import Home from "components/Home/Home.jsx";
+import Phonebook from "pages/Contacts/Phonebook.jsx";
+import Register from "pages/Register/Register";
+import Login from "pages/Login/Login";
+import NotFound from "pages/NotFound /NotFound";
 
 function App ( ){
   return (
-        <div className="App">
-          <Form />
-          <Filter />
-          <Contacts />
-        </div>
+      <div className="App">
+          <Routes>
+            <Route path="/" element={<Navigation />}>
+                <Route index element={<Home />} />
+                <Route path="contacts" element={<Phonebook />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+      </div>
   );
 }
 
